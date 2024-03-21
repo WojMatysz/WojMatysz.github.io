cb308ab50e2598e10a34bacc4f26f0a8a44ba448
Wojtek <matyszkiewicz.wojciech@gmail.com>
2024-03-21 23:14:42 +0100

    Supporting for different button captions depending on the selected language

--- scripts/pageLanguage.js
+++ scripts/pageLanguage.js
@@ -19,19 +19,40 @@ $(document).on('change', '.switch input:checked', function() {
 // Changing language of page
 function changeLanguageOfPage( fromLanguage, toLanguage ) {
 	changeLinksInButtons(fromLanguage, toLanguage);
-	changeCaptionsOnButtons(fromLanguage, toLanguage);
+	changeCaptionsOnButtons(toLanguage);
 	currentLanguageOfPage = toLanguage;
 };
 
 function changeLinksInButtons(fromLanguage, toLanguage) {
     $('button').each(function() {
-        let element = $(this);
-        let filePath = element.data("file");
+        let filePath = $(this).data("file");
         let newFilePath = filePath.replace("/" + fromLanguage + "/", "/" + toLanguage + "/");
-        element.data("file", newFilePath);
+		$(this).data("file", newFilePath);
     });
 };
 
-function changeCaptionsOnButtons(fromLanguage, toLanguage)
+async function changeCaptionsOnButtons(toLanguage) {
+	let json = await readJsonFile("lang/" + toLanguage + ".json");
+	changeCaptions(json);
+};
+
+async function readJsonFile(pathToFile) {
+	try {
+		let jsonFile = await fetch(pathToFile);
+		let json = await jsonFile.json();
+		return json;
+	} catch (error) {
+		console.log("Fail to read  or parse json file from path:" + pathToFile, error);
+		throw error;
+	}
+};
+
+function changeCaptions(json)
 {
+	$('button').each(function() {
+		let id = $(this).attr('id');
+		if(json.hasOwnProperty(id)) {
+			$(this).text(json[id]);
+		}
+	})
 };
